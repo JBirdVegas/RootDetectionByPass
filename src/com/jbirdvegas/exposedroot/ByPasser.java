f2f32e1f1230e2d5eb4dfdfe9866d8c3113c6686
JBirdVegas <jbirdvegas@gmail.com>
2014-05-18 17:37:29 -0500

    Add hook for RootTools root detection

--- src/com/jbirdvegas/exposedroot/ByPasser.java
+++ src/com/jbirdvegas/exposedroot/ByPasser.java
@@ -9,15 +9,23 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class ByPasser implements IXposedHookLoadPackage {
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log(String.format("App Loaded: %s", lpparam.packageName));
-        if (!lpparam.packageName.equals("com.jbirdvegas.rootkit")) {
-            return;
-        }
+        // Bypass my root detection
         XposedHelpers.findAndHookMethod("com.jbirdvegas.rootkit.RootKit",
                 lpparam.classLoader,
                 "isDeviceRooted",
                 Context.class,
+                new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        return false;
+                    }
+                });
+        // Bypass RootTools root detection
+        XposedHelpers.findAndHookMethod("com.stericson.RootTools.RootTools",
+                lpparam.classLoader,
+                "isRootAvailable",
                 new XC_MethodReplacement() {
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
